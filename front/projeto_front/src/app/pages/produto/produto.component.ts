3868df3be56c837e2db3a2f85ff40f4caf295967
Samanda Silva <samandasilva99@gmail.com>
2023-11-01 00:36:29 -0300

    Adição do sweetalert no método deletar

--- front/projeto_front/src/app/pages/produto/produto.component.ts
+++ front/projeto_front/src/app/pages/produto/produto.component.ts
@@ -1,6 +1,7 @@
 import { IProdutos } from './../../interfaces/produtos';
 import { Component } from '@angular/core';
 import { ProdutosService } from 'src/app/service/produtos.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-produto',
@@ -27,11 +28,27 @@ export class ProdutoComponent {
   }
 
   deletar(id: number) {
-    this.produtosService.deletar(id).subscribe(
-      () => {
-        this.produto = this.produto.filter((produto) => produto.id !== id);
-      }
-    );
-  }
+        Swal.fire({
+          title: 'Deseja excluir este produto?',
+          text: "Você não poderá reverter!",
+          icon: 'warning',
+          showCancelButton: true,
+          confirmButtonColor: '#3085d6',
+          cancelButtonColor: '#d33',
+          confirmButtonText: 'Sim'
+        }).then((result) => {
+          if (result.isConfirmed) {
+            this.produtosService.deletar(id).subscribe(
+              () => {
+                this.produto = this.produto.filter((produto) => produto.id !== id);
+              }
+            ); 
+              Swal.fire(
+              'Excluído!',
+              'Produto excluído.',
+              'success'
+            );         
+        }});
+}
 }
 
